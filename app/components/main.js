9ed90ddf254ff940fde08e48223ff59a3e7b7d44
AJ Srivastava <aj.srivastava@monsanto.com>
2016-09-21 08:24:39 -0500

    base style component added

--- app/components/main.js
+++ app/components/main.js
@@ -16,12 +16,13 @@ class Main extends Component{
 
   render(){
     return(
-      <div className="container">
+      <div className="container m-t-lg">
         <div className="row">
           <div className="col-md-12">
             <h2>X.O.F.</h2>
             <h4>UI/UX Pattern Guide and React-Redux boilerplate</h4>
             <p>The purpose of this project is to try and provide a set of reusable, extensible components for 1904Labs projects.</p>
+            <p><small><em>Eventually...</em></small></p>
           </div>
         </div>
       </div>
