0822ff56d8617faf87e21e70bdce10e6a41fbf60
AJ Srivastava <aj.srivastava@monsanto.com>
2016-09-21 13:23:52 -0500

    add link to footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
   return(
     <footer className="xof-footer text-center">
-      developed by the guys at 1904
+      developed by the guys at <a href="http://www.1904labs.com">1904Labs</a>
     </footer>
   )
 }
