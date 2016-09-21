b540e86ba2fea626d39a6c919a61c4d7dc7b6c96
AJ Srivastava <aj.srivastava@monsanto.com>
2016-09-21 13:43:01 -0500

    upcoming stuff

--- app/components/home.js
+++ app/components/home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import LoadingIcon from "./util/loadingIcon"
 
 class Home extends Component {
   constructor(props) {
@@ -12,13 +11,22 @@ class Home extends Component {
           <div className="row">
             <div className="col-md-12">
               <h1>X.O.F.</h1>
-              <h4>UI/UX Pattern Guide and React-Redux boilerplate</h4>
+              <h4>Pattern/Style Guide and React-Redux Boilerplate</h4>
               <p>The purpose of this project is to try and provide a set of reusable, extensible components for 1904Labs projects.</p>
               <p>Roadmap at the moment is to create a custom version of <a href="http://www.getbootstrap.com">Bootstrap</a>, tearing out the things we don't need and hopefully incorporating some cool things.</p>
             </div>
           </div>
           <div className="row">
             <div className="col-md-12">
+              <h4>Upcoming Features</h4>
+              <p>
+                <ul>
+                  <li>Additional grid options using flex and fallback</li>
+                  <li>Brand guidelines and style guide</li>
+                  <li>Patterns for reusable components</li>
+                  <li>Sample pages</li>
+                </ul>
+              </p>
             </div>
           </div>
         </div>
