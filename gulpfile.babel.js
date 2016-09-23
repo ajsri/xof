cc5c9c22701e597d5a1e0b0153fe97f4bdee4e19
AJ Srivastava <aj.srivastava@monsanto.com>
2016-09-23 10:54:06 -0500

    gulpfile sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,15 @@
 //don't judge me
+import gulp from "gulp"
+import sass from "gulp-sass"
+
+const dirs = {
+  src: "./app/styles",
+  dest: "./public/css"
+}
+
+gulp.task("default", () => {
+  return gulp.src("./app/styles/xof.scss"
+         .pipe(sass())
+         .pipe(gulp.dest("./public/css"))
+
+})
