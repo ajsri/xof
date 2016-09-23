19543e9c5b7e5f176235b01a2305f9625db501a2
AJ Srivastava <aj.srivastava@monsanto.com>
2016-09-23 09:00:39 -0500

    mongo integration

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
+//TODO: implement mongoose instead of native Mongo.Client
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost:27017/xof");
+
+const mongoUrl = "mongodb://localhost:27017/xof";
+const mongoClient = require("mongodb").MongoClient;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -27,7 +34,7 @@ app.use(webpackHotMiddleware(compiler, {
 
 app.use(express.static("public"));
 
-app.get("/*", function(req, res){
+app.get("/", function(req, res){
   res.sendFile(__dirname + "/public/index.html")
 })
 
@@ -36,6 +43,42 @@ app.post("/test", function(req, res){
   res.json({
     response: name + ", I can't believe you took orders from a button..."
   })
+})
+
+app.get("/users", function(req, res){
+  mongoClient.connect(mongoUrl, function(err, db){
+    if(err){
+      throw err;
+    }
+    else{
+      var users = db.collection("users").find().toArray(function(err, docs){
+        res.send(docs);
+        db.close()
+      });
+    }
+  })
+})
+
+app.post("/users", function(req, res){
+  if(!req.body.name){
+    res.json({
+      error: "no name defined"
+    })
+  }
+  else{
+    mongoClient.connect(mongoUrl, function(err, db){
+      if(err) {
+        throw err;
+      }
+      else{
+        db.collection("users").insert({name: req.body.name}, function(err, docs){
+          res.json({
+            message: "user " + req.body.name + " successfully added"
+          })
+        })
+      }
+    })
+  }
 })
 
 app.listen(3788, function(){
